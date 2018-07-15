43917336e68e1205fa684fa9254cfe64e78854bc
vdowsett <vdowsett@gmail.com>
2018-07-14 19:13:18 -0500

    adding script to cocktail order page

--- JavaScript102/assets/scripts/my-script.js
+++ JavaScript102/assets/scripts/my-script.js
@@ -1,6 +1,26 @@
+let numberOfDrinks = 0;
+console.log(numberOfDrinks);
+
+const submitOrder = function (name) {
+  numberOfDrinks++;
+
+  if (numberOfDrinks <= 4) {
+    document.querySelector('.order-details').innerHTML = name + " would a drink!"
+  }
+
+  else {
+    alert('Drink order queue is full.');
+  }
+  
+  updateOrderCount(numberOfDrinks);
+};
+
+const updateOrderCount = (count) => {
+  document.getElementById('drink-count').innerHTML = "Drinks Ordered: " + count;
+} ;
 
 document.addEventListener("DOMContentLoaded", function (event) {
-
-    document.getElementById('order-btn').onclick = function () {};
-
+  document.getElementById('order-btn').onclick = function () {
+      submitOrder(document.getElementById('order-form-input').value);
+    };
 });
